549d896ba7d0e92be09ec9c1fb61555d784c1c0c
MeghanaRenukunta <118656709+MeghanaRenukunta@users.noreply.github.com>
2023-01-09 22:43:53 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName("column");
+
+var ed=["hi","hello","ev1","maggi"];
+for(var i=0;i<elements.length;i++){
+    elements[i].innerHTML=ed[i];
+    document.getElementById("e8").innerHTML="<p>dxfgchgvjhkj</p>";
+}
+document.getElementById("hi").innerHTML="dxfchgvjhkj";
